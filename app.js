439e54decb1849ac2f391f7f133ac9208393f7cd
59pablo95 <64516593+59pablo95@users.noreply.github.com>
2023-08-30 06:51:28 -0300

    para Render

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{console.log("Servidor en ejecucion");});
+app.listen(port, function(){
+    console.log(`Servidor ejecutandose en el puerto  ${port}`);});
 
 app.get("/",(req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
